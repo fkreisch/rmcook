c4f78c87a0f278eed4a011bf0487c05d3602ce88
fkreisch <fkreisch@gmail.com>
2019-06-23 11:33:04 +0200

    new menu, jumbotron design

--- src/app/jumbotron/jumbotron.component.ts
+++ src/app/jumbotron/jumbotron.component.ts
@@ -8,13 +8,14 @@ import { Component, OnInit } from '@angular/core';
 export class JumbotronComponent implements OnInit {
 
   constructor() {
-    document.body.style.backgroundImage = "url('../../assets/img/chef.jpg')";
+    document.body.style.backgroundImage = 'url(\'../../assets/img/chef2.jpg\')';
   }
+
   ngOnDestroy() {
-    document.body.style.backgroundImage = "none";
+    document.body.style.backgroundImage = 'none';
   }
 
   ngOnInit() {
-    document.body.classList.add('[style.background-image]":"../../assets/img/chef.jpg');
+    document.body.classList.add('[style.background-image]":"../../assets/img/chef2.jpg');
   }
 }
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -11,7 +11,7 @@ export class NavbarComponent implements OnInit {
   toggleNavbar() {
     this.navbarOpen = !this.navbarOpen;
   }
-  
+
   constructor() { }
 
   ngOnInit() {
